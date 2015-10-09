4e5a41d2f6c2b113313c956e91fd6cf100f9e167
Alex Wilmer <alex.wilmer@oicr.on.ca>
2015-10-09 12:04:36 -0400

    test(travis): after script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo $BRANCH
-
-#   mkdir build
-#   mv * build
-#   tar -czf package.tgz build
-#   export SSHPASS=$DEPLOY_PASS
-#   sshpass -e scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
-#   sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/run.sh
+if [ "$TRAVIS_BRANCH" == "develop" ]; then
+  echo $TRAVIS_BRANCH
+  # mkdir build
+  # mv * build
+  # tar -czf package.tgz build
+  # export SSHPASS=$DEPLOY_PASS
+  # sshpass -e scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+  # sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/run.sh
+fi
