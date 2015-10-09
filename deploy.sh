6104d214f84359bc4ec80a929283df6cc10172fd
Alex Wilmer <alex.wilmer@oicr.on.ca>
2015-10-09 12:09:40 -0400

    test(travis): after script

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
   echo $TRAVIS_BRANCH
-  # mkdir build
-  # mv * build
-  # tar -czf package.tgz build
-  # export SSHPASS=$DEPLOY_PASS
-  # sshpass -e scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
-  # sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/run.sh
+  mkdir build
+  mv * build
+  tar -czf package.tgz build
+  export SSHPASS=$DEPLOY_PASS
+  sshpass -e scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+  sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/run.sh
 fi
--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo `dirname $0`
+
 cd `dirname $0`
 
 # Extract the package
 tar -xzf package.tgz
 rm package.tgz
+
+pwd
 
 # npm start
