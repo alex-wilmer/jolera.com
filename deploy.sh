bbe627a264fe6a06b2c1d38a75f6e9b4c2d066e7
Alex Wilmer <alex.wilmer@oicr.on.ca>
2015-10-09 11:40:36 -0400

    test(travis): after script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo Hello world!
+# set -e # exit with nonzero exit code if anything fails
+
+#!/bin/bash
+if [ "$BRANCH" == "develop" ]; then
+  mkdir build
+  mv * build
+  tar -czf package.tgz build
+  export SSHPASS=$DEPLOY_PASS
+  sshpass -e scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+  sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/run.sh
+fi
---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+# Extract the package
+tar -xzf package.tgz
+rm package.tgz
+
+# npm start
