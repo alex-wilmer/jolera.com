1ac3793be312ce025c17edbf6f0a409731394d09
Alex Wilmer <alex.wilmer@oicr.on.ca>
2015-10-09 12:39:16 -0400

    test(deploy): ssh files

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,10 @@ if [ "$TRAVIS_BRANCH" == "develop" ]; then
   mv * build
   tar -czf package.tgz build
 
-  scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
-  ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/run.sh
+  cat ~/.ssh/id_rsa.pub
+
+  # ssh user@domain "cat >> ~/.ssh/authorized_keys" < ~/.ssh/id_rsa.pub
+
+  # scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+  # ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/run.sh
 fi
