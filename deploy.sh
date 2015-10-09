f03a8292dc5c41719e7f71b8f81c107c37a6cb9a
Alex Wilmer <alex.wilmer@oicr.on.ca>
2015-10-09 12:29:09 -0400

    test(deploy): ssh files

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,7 @@ if [ "$TRAVIS_BRANCH" == "develop" ]; then
   mkdir build
   mv * build
   tar -czf package.tgz build
-  export SSHPASS=$DEPLOY_PASS
 
-  which sshpass
-  # sshpass -e scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
-  # sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/run.sh
+  scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+  ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/run.sh
 fi
