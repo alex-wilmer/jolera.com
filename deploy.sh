06a6aa7e203686db6933d1447fdeadbe3b153cfa
Alex Wilmer <alex.wilmer@oicr.on.ca>
2015-10-09 11:53:08 -0400

    test(travis): after script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# set -e # exit with nonzero exit code if anything fails
-
-#!/bin/bash
 echo $BRANCH
 
-# if [ "$BRANCH" == "develop" ]; then
 #   mkdir build
 #   mv * build
 #   tar -czf package.tgz build
 #   export SSHPASS=$DEPLOY_PASS
 #   sshpass -e scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 #   sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/run.sh
-# fi
